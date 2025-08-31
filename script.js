eb8f8958269c82dd291524824da69eaa69ff0e03
Srushti Sambare <srushtisambare@gmail.com>
2025-08-31 21:41:51 +0530

    Initial commit - portfolio site

---
+++ script.js
@@ -0,0 +1,9 @@
+// Mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+
